71a100b9cee1deae379a9da72a4c5d7ad86d0726
Augusto <acfb.app@gmail.com>
2022-07-25 16:54:44 -0300

    alteração nos testes

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,8 +1,44 @@
 import supertest from "supertest";
 import dotenv from "dotenv";
 import app from "../src/app.js";
+import prisma from "../src/database.js";
 
 dotenv.config();
+
+beforeAll(async () => {
+
+    await prisma.category.upsert({
+        where: { name: "Prática" },
+        update: {},
+        create: {
+          name: "Prática"
+        }
+    });
+    
+    await prisma.teacher.create({
+        data: {name: "Diego Pinho"}
+    });
+    
+    await prisma.term.create({
+        data: {
+          number: 2
+        }
+    });
+    
+    await prisma.discipline.create({
+        data: {
+          name: "React",
+          termId: 1
+        }
+    })
+
+    await prisma.teacherDiscipline.create({
+        data: {
+          teacherId: 1,
+          disciplineId: 1
+        }
+      })
+})
 
 describe("items test suite", () => {
 
@@ -17,6 +53,9 @@ describe("items test suite", () => {
   });
 
   it("create test", async () => {
+
+    const config = {Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1ODM4NjAyMn0.16F4EOwWBWb-SLOK-tM_MEU2PIXglx17uqdOzdmnZVk"};
+
     const body = {
         name: "prova 2",
         pdfUrl: "p2.pdf",
@@ -24,7 +63,8 @@ describe("items test suite", () => {
         teacher: "Diego Pinho",
         discipline: "React"
     }
-    const response = await supertest(app).post("/tests").send(body);
+
+    const response = await supertest(app).post("/tests").send(body).set(config);
     expect(response.statusCode).toEqual(201);
   });
 
@@ -34,14 +74,20 @@ describe("items test suite", () => {
   });
 
   it("get tests by disciplines", async () => {
-    const response = await supertest(app).get("/tests?groupBy=discipline");
+    const config = {Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1ODM4NjAyMn0.16F4EOwWBWb-SLOK-tM_MEU2PIXglx17uqdOzdmnZVk"};
+
+    const response = await supertest(app).get("/tests?groupBy=discipline").set(config);
     expect(response.statusCode).toEqual(201);
-    expect(response.body).toBe(JSON);
   });
 
   it("get tests by teachers", async () => {
-    const response = await supertest(app).get("/tests?groupBy=teacher");
+    const config = {Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1ODM4NjAyMn0.16F4EOwWBWb-SLOK-tM_MEU2PIXglx17uqdOzdmnZVk"};
+
+    const response = await supertest(app).get("/tests?groupBy=teacher").set(config);
     expect(response.statusCode).toEqual(201);
-    expect(response.body).toBe(JSON);
   });
+})
+
+afterAll(async () => {
+    await prisma.$disconnect();
 })
